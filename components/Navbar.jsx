81c9f5424ff8003c4a05be9cef8fa97705d881cf
andre361 <joeydre69@gmail.com>
2022-07-26 11:46:20 +0200

    created Link component to navigate routes in the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import Link from "./Link";
+import logo from "../public/logo.png";
 export default function Navbar() {
-  return <div className="bg-green-400 p-8"></div>;
+  return (
+    <div className="flex items-center bg-green-400 p-4 mb-2">
+      <span className="font-serif text-3xl text-gray-50 ">
+        <Link href="/">VALDIVIA</Link>
+      </span>
+      <span className="ml-auto ">
+        <Link className="text-gray-50 font-bold m-2 " href="/users/login">
+          Log In
+        </Link>
+        <Link className="text-gray-50 font-bold m-2" href="/users/register">
+          Register
+        </Link>
+      </span>
+    </div>
+  );
 }
